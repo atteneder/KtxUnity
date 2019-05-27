636f5d0050bf7822fb6b9298308eed7d020e6634
Andreas Atteneder <andreas.atteneder@gmail.com>
2019-05-27 21:54:01 +0200

    Adding Unity test project

---
+++ BasisUniversalUnity/Assets/Scripts/BasisLoader.cs
@@ -0,0 +1,88 @@
+﻿#if !(UNITY_ANDROID || UNITY_WEBGL) || UNITY_EDITOR
+#define LOCAL_LOADING
+#endif
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using System.IO;
+
+public class BasisLoader : MonoBehaviour
+{
+    public string filePath;
+    public Texture2D texture;
+
+    void Start() {
+        StartCoroutine(LoadBasisFile());
+    }
+
+    IEnumerator LoadBasisFile() {
+ 
+        var path = Path.Combine(Application.streamingAssetsPath,filePath);
+
+#if LOCAL_LOADING
+        path = string.Format( "file://{0}", path );
+#endif
+
+        var webRequest = UnityWebRequest.Get(path);
+        yield return webRequest.SendWebRequest();
+        if(!string.IsNullOrEmpty(webRequest.error)) {
+            yield break;
+        }
+        var bytes = webRequest.downloadHandler.data;
+
+        Debug.LogFormat("loaded {0} bytes", bytes.Length);
+
+        var basis = BasisUniversal.LoadBytes(bytes);
+
+        uint width;
+        uint height;
+
+        basis.GetImageSize(out width, out height);
+        Debug.LogFormat("image size {0} x {1}", width, height);
+
+        bool hasAlpha = basis.GetHasAlpha();
+        Debug.LogFormat("image has alpha {0}",hasAlpha);
+
+        var imageCount = basis.GetImageCount();
+        Debug.LogFormat("image count {0}",imageCount);
+
+        for(uint i=0; i<imageCount;i++) {
+            var levelCount = basis.GetLevelCount(i);
+            Debug.LogFormat("level count image {0}: {1}",i,levelCount);
+        }
+
+        TextureFormat tf = TextureFormat.DXT1;
+        BasisUniversal.TranscodeFormat transF = BasisUniversal.TranscodeFormat.BC1;
+
+        if(hasAlpha) {
+            if(SystemInfo.SupportsTextureFormat(TextureFormat.DXT5)) {
+                tf=TextureFormat.DXT5;
+                transF = BasisUniversal.TranscodeFormat.BC3;
+            } else {
+                Debug.LogError("No supported texture format found");
+            }
+        } else {
+            if(SystemInfo.SupportsTextureFormat(TextureFormat.DXT1)) {
+                tf=TextureFormat.DXT1;
+                transF = BasisUniversal.TranscodeFormat.BC1;
+            } else {
+                Debug.LogError("No supported texture format found");
+            }
+        }
+        byte[] trData;
+        if(basis.Transcode(0,0,transF,out trData)) {
+            Debug.LogFormat("transcoded {0} bytes", trData.Length);
+
+            texture = new Texture2D((int)width,(int)height,tf,false);
+            texture.LoadRawTextureData(trData);
+            texture.Apply();
+
+            var renderer = GetComponent<Renderer>();
+            if(renderer!=null && renderer.sharedMaterial!=null) {
+                renderer.material.mainTexture = texture;
+            }
+        }
+    }
+}
---
+++ BasisUniversalUnity/Assets/Scripts/BasisUniversal.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System;
+using UnityEngine;
+
+public class BasisUniversal
+{
+#if UNITY_EDITOR || UNITY_WEBGL || UNITY_IOS
+    const string INTERFACE_DLL = "__Internal";
+#elif UNITY_ANDROID || UNITY_STANDALONE
+    public const string INTERFACE_DLL = "basisu";
+#endif
+
+    // source: C++ enum transcoder_texture_format
+    public enum TranscodeFormat : uint {
+        ETC1,
+        BC1,
+        BC4,
+        PVRTC1_4_OPAQUE_ONLY,
+        BC7_M6_OPAQUE_ONLY,
+        ETC2,
+        BC3,
+        BC5,
+    }
+
+    public class BasisTexture {
+        IntPtr nativeReference;
+
+        public BasisTexture( IntPtr nativeReference ) {
+            this.nativeReference = nativeReference;
+        }
+
+        public bool GetHasAlpha() {
+            return aa_getHasAlpha(nativeReference);
+        }
+
+        public uint GetImageCount() {
+            return aa_getNumImages(nativeReference);
+        }
+
+        public uint GetLevelCount(uint imageIndex) {
+            return aa_getNumLevels(nativeReference,imageIndex);
+        }
+
+        public void GetImageSize( out uint width, out uint height, System.UInt32 image_index = 0, System.UInt32 level_index = 0) {
+            width = aa_getImageWidth(nativeReference,image_index,level_index);
+            height = aa_getImageHeight(nativeReference,image_index,level_index);
+        }
+
+        uint GetImageTranscodedSize(uint imageIndex, uint levelIndex, TranscodeFormat format) {
+            return aa_getImageTranscodedSizeInBytes(nativeReference,imageIndex,levelIndex,(uint)format);
+        }
+
+        public unsafe bool Transcode(uint imageIndex,uint levelIndex,TranscodeFormat format,out byte[] transcodedData ) {
+            
+            transcodedData = null;
+
+            if(!aa_startTranscoding(nativeReference)) {
+                return false;
+            }
+
+            var size = GetImageTranscodedSize(imageIndex,levelIndex,format);
+            byte[] data = new byte[size];
+
+            bool result = false;
+            fixed( void* dst = &(data[0]) ) {
+                result = aa_transcodeImage(nativeReference,dst,size,imageIndex,levelIndex,(uint)format,0,0);
+            }
+            transcodedData = data;
+            return result;
+        }
+    }
+
+    static bool initialized;
+
+    static void Init()
+    {
+        initialized=true;
+        aa_basis_init();
+    }
+
+    public static unsafe BasisTexture LoadBytes( byte[] data ) {
+        if(!initialized) {
+            Init();
+        }
+        fixed( void* src = &(data[0]) ) {
+            return new BasisTexture(aa_create_basis(src,data.Length));
+        }
+    }
+
+    [DllImport(INTERFACE_DLL)]
+    private static extern void aa_basis_init();
+
+    [DllImport(INTERFACE_DLL)]
+    private static unsafe extern System.IntPtr aa_create_basis( void * data, int length );
+
+    [DllImport(INTERFACE_DLL)]
+    private static extern void aa_delete_basis( IntPtr basis );
+
+    [DllImport(INTERFACE_DLL)]
+    private static extern bool aa_getHasAlpha( IntPtr basis );
+
+    [DllImport(INTERFACE_DLL)]
+    private static extern System.UInt32 aa_getNumImages( IntPtr basis );
+
+    [DllImport(INTERFACE_DLL)]
+    private static extern System.UInt32 aa_getNumLevels( IntPtr basis, System.UInt32 image_index);
+
+    [DllImport(INTERFACE_DLL)]
+    private static extern System.UInt32 aa_getImageWidth( IntPtr basis, System.UInt32 image_index, System.UInt32 level_index);
+
+    [DllImport(INTERFACE_DLL)]
+    private static extern System.UInt32 aa_getImageHeight( IntPtr basis, System.UInt32 image_index, System.UInt32 level_index);
+
+    [DllImport(INTERFACE_DLL)]
+    private static extern System.UInt32 aa_getImageTranscodedSizeInBytes( IntPtr basis, System.UInt32 image_index, System.UInt32 level_index, System.UInt32 format);
+
+    [DllImport(INTERFACE_DLL)]
+    private static extern bool aa_startTranscoding( IntPtr basis );
+
+    [DllImport(INTERFACE_DLL)]
+    private static unsafe extern bool aa_transcodeImage( IntPtr basis, void * dst, uint dst_size, System.UInt32 image_index, System.UInt32 level_index, System.UInt32 format, System.UInt32 pvrtc_wrap_addressing, System.UInt32 get_alpha_for_opaque_formats);
+
+}
