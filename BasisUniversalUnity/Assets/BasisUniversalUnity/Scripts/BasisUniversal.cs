7acbcda828b3111f908b78fa10b3688ecdfac369
Andreas Atteneder <andreas.atteneder@gmail.com>
2019-06-21 01:42:36 +0200

    Using new experimental GraphicsFormat for choosing texture format (now works on some iOS devices). Kept TextureFormat as a fallback. Moved format decision into BasisUniversal class.

--- BasisUniversalUnity/Assets/BasisUniversalUnity/Scripts/BasisUniversal.cs
+++ BasisUniversalUnity/Assets/BasisUniversalUnity/Scripts/BasisUniversal.cs
@@ -16,6 +16,7 @@ using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using System;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering;
 #if BASISU_VERBOSE
 using System.Text;
 #endif
@@ -92,8 +93,10 @@ namespace BasisUniversalUnity {
         }
 
         static bool initialized;
-        static Dictionary<TextureFormat,BasisUniversal.TranscodeFormat> opaqueFormatDict;
-        static Dictionary<TextureFormat,BasisUniversal.TranscodeFormat> alphaFormatDict;
+        static Dictionary<GraphicsFormat,BasisUniversal.TranscodeFormat> opaqueFormatDict;
+        static Dictionary<GraphicsFormat,BasisUniversal.TranscodeFormat> alphaFormatDict;
+        static Dictionary<TextureFormat,BasisUniversal.TranscodeFormat> opaqueFormatLegacyDict;
+        static Dictionary<TextureFormat,BasisUniversal.TranscodeFormat> alphaFormatLegacyDict;
 
         public static void Init() {
             if(!initialized) {
@@ -104,22 +107,40 @@ namespace BasisUniversalUnity {
         {
             initialized=true;
             aa_basis_init();
+
             if(opaqueFormatDict==null) {
-                opaqueFormatDict = new Dictionary<TextureFormat, BasisUniversal.TranscodeFormat>();
-                opaqueFormatDict.Add(TextureFormat.BC7,BasisUniversal.TranscodeFormat.BC7_M6_OPAQUE_ONLY);
-                opaqueFormatDict.Add(TextureFormat.PVRTC_RGB4,BasisUniversal.TranscodeFormat.PVRTC1_4_OPAQUE_ONLY);
-                opaqueFormatDict.Add(TextureFormat.ETC_RGB4,BasisUniversal.TranscodeFormat.ETC1);
-                opaqueFormatDict.Add(TextureFormat.ETC2_RGBA8,BasisUniversal.TranscodeFormat.ETC2);
-                opaqueFormatDict.Add(TextureFormat.DXT1,BasisUniversal.TranscodeFormat.BC1);
-                opaqueFormatDict.Add(TextureFormat.BC4,BasisUniversal.TranscodeFormat.BC4);
-                opaqueFormatDict.Add(TextureFormat.BC5,BasisUniversal.TranscodeFormat.BC5);
+                opaqueFormatDict = new Dictionary<GraphicsFormat, BasisUniversal.TranscodeFormat>();
+                opaqueFormatDict.Add(GraphicsFormat.RGBA_BC7_SRGB,BasisUniversal.TranscodeFormat.BC7_M6_OPAQUE_ONLY);
+                opaqueFormatDict.Add(GraphicsFormat.RGB_PVRTC_4Bpp_SRGB,BasisUniversal.TranscodeFormat.PVRTC1_4_OPAQUE_ONLY);
+                opaqueFormatDict.Add(GraphicsFormat.RGB_ETC_UNorm,BasisUniversal.TranscodeFormat.ETC1);
+                opaqueFormatDict.Add(GraphicsFormat.RGB_ETC2_SRGB,BasisUniversal.TranscodeFormat.ETC2);
+                opaqueFormatDict.Add(GraphicsFormat.RGBA_DXT1_SRGB,BasisUniversal.TranscodeFormat.BC1);
+                opaqueFormatDict.Add(GraphicsFormat.R_BC4_UNorm,BasisUniversal.TranscodeFormat.BC4);
+                opaqueFormatDict.Add(GraphicsFormat.RG_BC5_UNorm,BasisUniversal.TranscodeFormat.BC5);
             }
 
             if(alphaFormatDict==null) {
-                alphaFormatDict = new Dictionary<TextureFormat, BasisUniversal.TranscodeFormat>();
-                alphaFormatDict.Add(TextureFormat.DXT5,BasisUniversal.TranscodeFormat.BC3);
-                alphaFormatDict.Add(TextureFormat.ETC2_RGBA8,BasisUniversal.TranscodeFormat.ETC2);            
-                alphaFormatDict.Add(TextureFormat.ETC2_RGBA1,BasisUniversal.TranscodeFormat.ETC2); // Not sure if this works
+                alphaFormatDict = new Dictionary<GraphicsFormat, BasisUniversal.TranscodeFormat>();
+                alphaFormatDict.Add(GraphicsFormat.RGBA_DXT5_SRGB,BasisUniversal.TranscodeFormat.BC3);
+                alphaFormatDict.Add(GraphicsFormat.RGBA_ETC2_SRGB,BasisUniversal.TranscodeFormat.ETC2);
+            }
+
+            if(opaqueFormatLegacyDict==null) {
+                opaqueFormatLegacyDict = new Dictionary<TextureFormat, BasisUniversal.TranscodeFormat>();
+                opaqueFormatLegacyDict.Add(TextureFormat.BC7,BasisUniversal.TranscodeFormat.BC7_M6_OPAQUE_ONLY);
+                opaqueFormatLegacyDict.Add(TextureFormat.PVRTC_RGB4,BasisUniversal.TranscodeFormat.PVRTC1_4_OPAQUE_ONLY);
+                opaqueFormatLegacyDict.Add(TextureFormat.ETC_RGB4,BasisUniversal.TranscodeFormat.ETC1);
+                opaqueFormatLegacyDict.Add(TextureFormat.ETC2_RGBA8,BasisUniversal.TranscodeFormat.ETC2);
+                opaqueFormatLegacyDict.Add(TextureFormat.DXT1,BasisUniversal.TranscodeFormat.BC1);
+                opaqueFormatLegacyDict.Add(TextureFormat.BC4,BasisUniversal.TranscodeFormat.BC4);
+                opaqueFormatLegacyDict.Add(TextureFormat.BC5,BasisUniversal.TranscodeFormat.BC5);
+            }
+
+            if(alphaFormatLegacyDict==null) {
+                alphaFormatLegacyDict = new Dictionary<TextureFormat, BasisUniversal.TranscodeFormat>();
+                alphaFormatLegacyDict.Add(TextureFormat.DXT5,BasisUniversal.TranscodeFormat.BC3);
+                alphaFormatLegacyDict.Add(TextureFormat.ETC2_RGBA8,BasisUniversal.TranscodeFormat.ETC2);            
+                alphaFormatLegacyDict.Add(TextureFormat.ETC2_RGBA1,BasisUniversal.TranscodeFormat.ETC2); // Not sure if this works
             }
 
 #if BASISU_VERBOSE
@@ -127,39 +148,159 @@ namespace BasisUniversalUnity {
 #endif
         }
 
-        public static unsafe BasisTexture LoadBytes( byte[] data ) {
+        public static unsafe Texture2D LoadBytes( byte[] data ) {
             if(!initialized) {
                 InitInternal();
             }
+            
+            Log("loading {0} bytes", data.Length);
+
+            BasisTexture basis;
             fixed( void* src = &(data[0]) ) {
-                return new BasisTexture(aa_create_basis(src,data.Length));
+                basis = new BasisTexture(aa_create_basis(src,data.Length));
             }
+            uint width;
+            uint height;
+
+            basis.GetImageSize(out width, out height);
+            Log("image size {0} x {1}", width, height);
+
+            bool hasAlpha = basis.GetHasAlpha();
+            Log("image has alpha {0}",hasAlpha);
+
+            var imageCount = basis.GetImageCount();
+            Log("image count {0}",imageCount);
+
+            for(uint i=0; i<imageCount;i++) {
+                var levelCount = basis.GetLevelCount(i);
+                Log("level count image {0}: {1}",i,levelCount);
+            }
+
+            GraphicsFormat gf;
+            TextureFormat tf;
+            TranscodeFormat transF = TranscodeFormat.ETC1;
+            Texture2D texture = null;
+
+            if(hasAlpha) {
+                if(BasisUniversal.GetPreferredFormatAlpha(out gf,out transF)) {
+                    Log("Transcode to {0} (alpha)",gf);
+                    texture = new Texture2D((int)width,(int)height,gf,TextureCreationFlags.None);
+                } else
+                if(BasisUniversal.GetPreferredFormatLegacyAlpha(out tf,out transF)) {
+                    Log("Transcode to {0} (legacy alpha)",tf);
+                    texture = new Texture2D((int)width,(int)height,tf,false);
+                }
+            }
+            
+            if( !hasAlpha || texture==null ) {
+                if(BasisUniversal.GetPreferredFormat(out gf,out transF)) {
+                    Log("Transcode to {0}",gf);
+                    texture = new Texture2D((int)width,(int)height,gf,TextureCreationFlags.None);
+                } else
+                if(BasisUniversal.GetPreferredFormatLegacy(out tf,out transF)) {
+                    Log("Transcode to {0} (legacy)",tf);
+                    texture = new Texture2D((int)width,(int)height,tf,false);
+                }
+            }
+
+            if(texture==null) {
+                Debug.LogError("No supported format found!\nRebuild with BASISU_VERBOSE scripting define to debug.");
+                #if BASISU_VERBOSE
+                BasisUniversal.CheckTextureSupport();
+                #endif
+                return null;
+            }
+
+            Log("Transcode to basisu {0}",transF);
+            byte[] trData;
+            if(basis.Transcode(0,0,transF,out trData)) {
+                // Log("transcoded {0} bytes", trData.Length);
+                texture.LoadRawTextureData(trData);
+                texture.Apply();
+                return texture;
+            }
+            return null;
         }
 
 #if BASISU_VERBOSE
         public static void CheckTextureSupport() {
             var sb = new StringBuilder();
             foreach(var format in opaqueFormatDict) {
-                var supported = SystemInfo.SupportsTextureFormat(format.Key);
+                var supported = SystemInfo.IsFormatSupported(format.Key,FormatUsage.Sample);
                 sb.AppendFormat("{0} support: {1}\n",format.Key,supported);
             }
             foreach(var format in alphaFormatDict) {
-                var supported = SystemInfo.SupportsTextureFormat(format.Key);
+                var supported = SystemInfo.IsFormatSupported(format.Key,FormatUsage.Sample);
                 sb.AppendFormat("(alpha) {0} support: {1}\n",format.Key,supported);
             }
+            foreach(var format in opaqueFormatLegacyDict) {
+                var supported = SystemInfo.SupportsTextureFormat(format.Key);
+                sb.AppendFormat("legacy {0} support: {1}\n",format.Key,supported);
+            }
+            foreach(var format in alphaFormatLegacyDict) {
+                var supported = SystemInfo.SupportsTextureFormat(format.Key);
+                sb.AppendFormat("legacy (alpha) {0} support: {1}\n",format.Key,supported);
+            }
+
+            GraphicsFormat[] allGfxFormats = (GraphicsFormat[]) Enum.GetValues(typeof(GraphicsFormat));
+            foreach(var format in allGfxFormats) {
+                sb.AppendFormat(
+                    "{0} sample:{1} blend:{2} getpixels:{3} linear:{4} loadstore:{5} aa2:{6} aa4:{7} aa8:{8} readpixels:{9} render:{10} setpixels:{11} sparse:{12}\n"
+                    ,format
+                    ,SystemInfo.IsFormatSupported(format,FormatUsage.Sample)?"1":"0"
+                    ,SystemInfo.IsFormatSupported(format,FormatUsage.Blend)?"1":"0"
+                    ,SystemInfo.IsFormatSupported(format,FormatUsage.GetPixels)?"1":"0"
+                    ,SystemInfo.IsFormatSupported(format,FormatUsage.Linear)?"1":"0"
+                    ,SystemInfo.IsFormatSupported(format,FormatUsage.LoadStore)?"1":"0"
+                    ,SystemInfo.IsFormatSupported(format,FormatUsage.MSAA2x)?"1":"0"
+                    ,SystemInfo.IsFormatSupported(format,FormatUsage.MSAA4x)?"1":"0"
+                    ,SystemInfo.IsFormatSupported(format,FormatUsage.MSAA8x)?"1":"0"
+                    ,SystemInfo.IsFormatSupported(format,FormatUsage.ReadPixels)?"1":"0"
+                    ,SystemInfo.IsFormatSupported(format,FormatUsage.Render)?"1":"0"
+                    ,SystemInfo.IsFormatSupported(format,FormatUsage.SetPixels)?"1":"0"
+                    ,SystemInfo.IsFormatSupported(format,FormatUsage.Sparse)?"1":"0"
+                );
+            }
+
             Debug.Log(sb.ToString());
         }
 #endif
 
-        public static bool GetPreferredFormat( out TextureFormat unityFormat, out TranscodeFormat transcodeFormat, bool hasAlpha = false ) {
+        public static bool GetPreferredFormat( out GraphicsFormat unityFormat, out TranscodeFormat transcodeFormat ) {
+            unityFormat = GraphicsFormat.RGBA_DXT1_SRGB;
+            transcodeFormat = TranscodeFormat.BC1;
+
+            foreach(var format in opaqueFormatDict) {
+                var supported = SystemInfo.IsFormatSupported(format.Key,FormatUsage.Sample);
+                if (supported) {
+                    unityFormat = format.Key;
+                    transcodeFormat = format.Value;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool GetPreferredFormatAlpha( out GraphicsFormat unityFormat, out TranscodeFormat transcodeFormat ) {
+            unityFormat = GraphicsFormat.RGBA_DXT1_SRGB;
+            transcodeFormat = TranscodeFormat.BC1;
+
+            foreach(var format in alphaFormatDict) {
+                var supported = SystemInfo.IsFormatSupported(format.Key,FormatUsage.Sample);
+                if (supported) {
+                    unityFormat = format.Key;
+                    transcodeFormat = format.Value;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool GetPreferredFormatLegacy( out TextureFormat unityFormat, out TranscodeFormat transcodeFormat ) {
             unityFormat = TextureFormat.DXT1;
             transcodeFormat = TranscodeFormat.BC1;
-            
-            var formats = hasAlpha
-                ? alphaFormatDict
-                : opaqueFormatDict;
 
-            foreach(var format in formats) {
+            foreach(var format in opaqueFormatLegacyDict) {
                 var supported = SystemInfo.SupportsTextureFormat(format.Key);
                 if (supported) {
                     unityFormat = format.Key;
@@ -167,15 +308,27 @@ namespace BasisUniversalUnity {
                     return true;
                 }
             }
-            if(hasAlpha) {
-                // Fallback to opaque texture format
-                var opaqueFound = GetPreferredFormat(out unityFormat,out transcodeFormat, false);
-                if(opaqueFound) {
-                    Debug.LogWarningFormat("No supported alpha format found. Fallback to opaque format {0} ({1})",transcodeFormat,unityFormat);
+            return false;
+        }
+
+        public static bool GetPreferredFormatLegacyAlpha( out TextureFormat unityFormat, out TranscodeFormat transcodeFormat ) {
+            unityFormat = TextureFormat.DXT1;
+            transcodeFormat = TranscodeFormat.BC1;
+
+            foreach(var format in alphaFormatLegacyDict) {
+                var supported = SystemInfo.SupportsTextureFormat(format.Key);
+                if (supported) {
+                    unityFormat = format.Key;
+                    transcodeFormat = format.Value;
+                    return true;
                 }
-                return opaqueFound;
             }
             return false;
+        }
+
+        [System.Diagnostics.Conditional("BASISU_VERBOSE")]
+        static void Log(string format, params object[] args) {
+            Debug.LogFormat(format,args);
         }
 
         [DllImport(INTERFACE_DLL)]
--- BasisUniversalUnity/Assets/BasisUniversalUnity/Scripts/BasisUniversalTexture.cs
+++ BasisUniversalUnity/Assets/BasisUniversalUnity/Scripts/BasisUniversalTexture.cs
@@ -51,57 +51,11 @@ namespace BasisUniversalUnity {
             }
             var bytes = webRequest.downloadHandler.data;
 
-            Log("loaded {0} bytes", bytes.Length);
+            var texture = BasisUniversal.LoadBytes(bytes);
 
-            var basis = BasisUniversal.LoadBytes(bytes);
-
-            uint width;
-            uint height;
-
-            basis.GetImageSize(out width, out height);
-            Log("image size {0} x {1}", width, height);
-
-            bool hasAlpha = basis.GetHasAlpha();
-            Log("image has alpha {0}",hasAlpha);
-
-            var imageCount = basis.GetImageCount();
-            Log("image count {0}",imageCount);
-
-            for(uint i=0; i<imageCount;i++) {
-                var levelCount = basis.GetLevelCount(i);
-                Log("level count image {0}: {1}",i,levelCount);
+            if(onTextureLoaded!=null) {
+                onTextureLoaded(texture);
             }
-
-            TextureFormat tf;
-            BasisUniversal.TranscodeFormat transF;
-
-            if(!BasisUniversal.GetPreferredFormat(out tf,out transF,hasAlpha)) {
-                Debug.LogError("No supported format found!\nRebuild with BASISU_VERBOSE scripting define to debug.");
-                #if BASISU_VERBOSE
-                BasisUniversal.CheckTextureSupport();
-                #endif
-                yield break;
-            } else {
-                Log("Trying to transcode to {0} ({1})",tf,transF);
-            }
-
-            byte[] trData;
-            if(basis.Transcode(0,0,transF,out trData)) {
-                Log("transcoded {0} bytes", trData.Length);
-
-                var texture = new Texture2D((int)width,(int)height,tf,false);
-                texture.LoadRawTextureData(trData);
-                texture.Apply();
-
-                if(onTextureLoaded!=null) {
-                    onTextureLoaded(texture);
-                }
-            }
-        }
-
-        [System.Diagnostics.Conditional("BASISU_VERBOSE")]
-        void Log(string format, params object[] args) {
-            Debug.LogFormat(format,args);
         }
     }
 }
