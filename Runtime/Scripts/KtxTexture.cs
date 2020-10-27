77f374cc2d2c2cf0a871147db4782a305ae5ba3a
Andreas Atteneder <andreas.atteneder@gmail.com>
2020-10-28 00:18:21 +0100

    chore: Moved transcoding into (sub) function

--- Runtime/Scripts/KtxTexture.cs
+++ Runtime/Scripts/KtxTexture.cs
@@ -21,72 +21,75 @@ using Unity.Collections;
 namespace KtxUnity {
     public class KtxTexture : TextureBase
     {
+        Texture2D texture;
+
         public override IEnumerator LoadBytesRoutine(NativeSlice<byte> data, bool linear = false) {
 
-            Texture2D texture = null;
             var orientation = TextureOrientation.UNITY_DEFAULT;
 
             var ktx = new KtxNativeInstance(data);
 
             if(ktx.valid) {
-
-                // TODO: Maybe do this somewhere more central
-                TranscodeFormatHelper.Init();
-
                 orientation = ktx.orientation;
-
-                var formats = GetFormat(ktx,ktx,linear);
-
-                if(formats.HasValue) {
-                    var gf = formats.Value.format;
-#if KTX_VERBOSE
-                    Debug.LogFormat("Transcode to GraphicsFormat {0} ({1})",gf,formats.Value.transcodeFormat);
-#endif
-                    Profiler.BeginSample("KtxTranscode");
-
-                    var job = new KtxTranscodeJob();
-                    
-                    var jobHandle = ktx.LoadBytesJob(
-                        ref job,
-                        formats.Value.transcodeFormat
-                        );
-
-                    Profiler.EndSample();
-                    
-                    while(!jobHandle.IsCompleted) {
-                        yield return null;
-                    }
-                    jobHandle.Complete();
-
-                    if(job.result[0] == KtxErrorCode.KTX_SUCCESS) {
-                        Profiler.BeginSample("LoadBytesRoutineGPUupload");
-                        uint width = ktx.baseWidth;
-                        uint height = ktx.baseHeight;
-
-                        if(formats.Value.format== GraphicsFormat.RGBA_DXT5_SRGB && !ktx.hasAlpha) {
-                            // ktx library automatically decides to use the smaller DXT1 instead of DXT5 if no alpha
-#if UNITY_2018_3_OR_NEWER
-                            gf = GraphicsFormat.RGBA_DXT1_SRGB;
-#else
-                            gf = GraphicsFormat.RGB_DXT1_SRGB;
-#endif
-                        }
-                        try {
-                            texture = ktx.LoadTextureData(gf);
-                        }
-                        catch (UnityException) {
-                            Debug.LogError(ERR_MSG_TRANSCODE_FAILED);
-                            texture = null;
-                        }
-                        Profiler.EndSample();
-                    } else {
-                        Debug.LogError(ERR_MSG_TRANSCODE_FAILED);
-                    }
-                    job.result.Dispose();
-                }
+                yield return Transcode(ktx,linear);
             }
             ktx.Unload();
             OnTextureLoaded(texture,orientation);
+        }
+
+        IEnumerator Transcode(KtxNativeInstance ktx, bool linear) {
+            // TODO: Maybe do this somewhere more central
+            TranscodeFormatHelper.Init();
+
+            var formats = GetFormat(ktx,ktx,linear);
+
+            if(formats.HasValue) {
+                var gf = formats.Value.format;
+#if KTX_VERBOSE
+                Debug.LogFormat("Transcode to GraphicsFormat {0} ({1})",gf,formats.Value.transcodeFormat);
+#endif
+                Profiler.BeginSample("KtxTranscode");
+
+                var job = new KtxTranscodeJob();
+                
+                var jobHandle = ktx.LoadBytesJob(
+                    ref job,
+                    formats.Value.transcodeFormat
+                    );
+
+                Profiler.EndSample();
+                
+                while(!jobHandle.IsCompleted) {
+                    yield return null;
+                }
+                jobHandle.Complete();
+
+                if(job.result[0] == KtxErrorCode.KTX_SUCCESS) {
+                    Profiler.BeginSample("LoadBytesRoutineGPUupload");
+                    uint width = ktx.baseWidth;
+                    uint height = ktx.baseHeight;
+
+                    if(formats.Value.format== GraphicsFormat.RGBA_DXT5_SRGB && !ktx.hasAlpha) {
+                        // ktx library automatically decides to use the smaller DXT1 instead of DXT5 if no alpha
+#if UNITY_2018_3_OR_NEWER
+                        gf = GraphicsFormat.RGBA_DXT1_SRGB;
+#else
+                        gf = GraphicsFormat.RGB_DXT1_SRGB;
+#endif
+                    }
+                    try {
+                        texture = ktx.LoadTextureData(gf);
+                    }
+                    catch (UnityException) {
+                        Debug.LogError(ERR_MSG_TRANSCODE_FAILED);
+                        texture = null;
+                    }
+                    Profiler.EndSample();
+                } else {
+                    Debug.LogError(ERR_MSG_TRANSCODE_FAILED);
+                }
+                job.result.Dispose();
+            }
         }
     }
 }
